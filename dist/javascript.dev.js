f9d0b35337b2b786ce607b6858f46355a15e60dc
Marinho <158611550+MarinhoRapha@users.noreply.github.com>
2024-11-25 18:59:39 -0300

    Add files via upload

---
+++ dist/javascript.dev.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var xpHero = 0;
+var heroName = 'Rapha';
+
+function xpCalc(xp) {
+  xpHero = xp;
+
+  if (xpHero <= 1000) {
+    xpLevel = 'Ferro';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  } else if (xpHero > 1000 && xpHero <= 2000) {
+    xpLevel = 'Bronze';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  } else if (xpHero > 2000 && xpHero <= 5999) {
+    xpLevel = 'Prata';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  } else if (xpHero >= 6000 && xpHero <= 7000) {
+    xpLevel = 'Ouro';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  } else if (xpHero > 7000 && xpHero <= 8000) {
+    xpLevel = 'Platina';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  } else if (xpHero > 8000 && xpHero <= 9000) {
+    xpLevel = 'Ascendente';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  } else if (xpHero > 9000 && xpHero <= 10000) {
+    xpLevel = 'Imortal';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  } else if (xpHero >= 10001) {
+    xpLevel = 'Radiante';
+    console.log('Ola, ' + heroName + '! Seu nivel de XP é ' + xpLevel);
+  }
+}
+
+xpCalc(1220);
